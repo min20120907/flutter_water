f35a843a48a27a680c9600d1a2fea665bafaac3b
Cangyu Lin <jefflin.je598@gmail.com>
2023-12-05 11:50:38 +0800

    first commit

---
+++ dashboard_data.dart
@@ -0,0 +1,22 @@
+  class DashboardData {
+    final double waterFlowSpeed;
+    final double airPressure;
+    final double feelLikeTemperature;
+    final double realTemperature;
+    final double humidity;
+    final double waterLevel;
+    final double totalWater;
+    final double time;
+
+    DashboardData({
+      required this.waterFlowSpeed,
+      required this.airPressure,
+      required this.feelLikeTemperature,
+      required this.realTemperature,
+      required this.humidity,
+      required this.waterLevel,
+      required this.totalWater,
+      required this.time,
+    });
+  }
+
---
+++ settings_data.dart
@@ -0,0 +1,14 @@
+  class SettingsData {
+    final String unitSettings;
+    final String updateTime;
+    final String updateInterval;
+    final int flowControlStatus;
+
+    SettingsData({
+      required this.unitSettings,
+      required this.updateTime,
+      required this.updateInterval,
+      required this.flowControlStatus,
+    });
+  }
+
