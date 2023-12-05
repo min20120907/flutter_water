06f079a87cabc09d1e4ff0496190149ae5ffcfec
Cangyu Lin <jefflin.je598@gmail.com>
2023-12-05 15:24:27 +0800

    fix the hiding buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_water/models/settings_data.dart';
+import 'package:flutter_water/models/settings_provider.dart';
+import 'package:flutter_water/providers/dashboard_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (ctx) => DashboardProvider()),
+      ChangeNotifierProvider(create: (ctx) => SettingsProvider()),
+    ],
+    child: const DashboardApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DashboardApp extends StatelessWidget {
+  const DashboardApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Dashboard(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final dashboardData =
+        Provider.of<DashboardProvider>(context, listen: false).dashboardData;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsView()),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: Provider.of<DashboardProvider>(context, listen: false)
+            .fetchDashboardData(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              itemCount: dashboardData.length,
+              itemBuilder: (ctx, i) => Card(
+                child: ListTile(
+                  leading: Text(
+                      'Water Flow Speed: ${dashboardData[i].waterFlowSpeed}'),
+                  title: Text('Air Pressure: ${dashboardData[i].airPressure}'),
+                  subtitle: Text(
+                      'Feel-like Temperature: ${dashboardData[i].feelLikeTemperature}'),
+                ),
+              ),
+            );
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SettingsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final settingsProvider =
+        Provider.of<SettingsProvider>(context, listen: false);
+
+    final settingsData =
+        Provider.of<DashboardProvider>(context, listen: false).settingsData;
+
+    final userIdController = TextEditingController(text: settingsData.userId);
+    final unitSettingsController =
+        TextEditingController(text: settingsData.unitSettings);
+    final updateTimeController =
+        TextEditingController(text: settingsData.updateTime);
+    final updateIntervalController =
+        TextEditingController(text: settingsData.updateInterval);
+    final flowControlStatusController =
+        TextEditingController(text: settingsData.flowControlStatus.toString());
+
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            controller: userIdController,
+            decoration: const InputDecoration(
+              labelText: 'User ID',
+            ),
+          ),
+          TextFormField(
+            controller: unitSettingsController,
+            decoration: const InputDecoration(
+              labelText: 'Unit Settings',
+            ),
+          ),
+          TextFormField(
+            controller: updateTimeController,
+            decoration: const InputDecoration(
+              labelText: 'Update Time',
+            ),
+          ),
+          TextFormField(
+            controller: updateIntervalController,
+            decoration: const InputDecoration(
+              labelText: 'Update Interval',
+            ),
+          ),
+          TextFormField(
+            controller: flowControlStatusController,
+            decoration: const InputDecoration(
+              labelText: 'Flow Control Status',
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('Update Settings'),
+            onPressed: () {
+              settingsProvider.updateSettings(
+                userIdController.text,
+                unitSettingsController.text,
+                updateTimeController.text,
+                updateIntervalController.text,
+                int.parse(flowControlStatusController.text),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
